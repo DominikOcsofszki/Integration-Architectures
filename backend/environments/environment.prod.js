830087042e4dfe17157d0cff7e029ecdd6e75906
Luca Ringhausen <luca.ringhausen@h-brs.de>
2023-11-02 18:10:14 +0100

    configured deployment

--- backend/environments/environment.prod.js
+++ backend/environments/environment.prod.js
@@ -5,10 +5,10 @@ const environment = {
     db:{
         host: 'iar-mongo.inf.h-brs.de',
         port: 27017,
-        username: 'iarframework',
-        password: 'iarframework!',
-        authSource: 'iarframework',
-        name: 'iarframework'
+        username: 'erasmux',
+        password: 'erasmux!',
+        authSource: 'erasmux',
+        name: 'erasmux'
     },
     corsOrigins: [
         'http://iar-frontend.inf.h-brs.de'
