24e671db1c1009b15ad4fe2d083e20e57253eab5
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2023-11-25 12:06:25 +0100

    updated salesman class

--- backend/src/model/Salesman.ts
+++ backend/src/model/Salesman.ts
@@ -1,19 +1,21 @@
 export class Salesman {
-    _id: number;
+    id: number;
     firstName: String;
     lastName: String;
-    email: String;
-    constructor(id: number, firstName: String, lastName: String, email: String,) {
-        this._id = id,
-            this.firstName = firstName,
-            this.lastName = lastName,
-            this.email = email;
+    department: String;
+
+    constructor(id: number, firstName: String, lastName: String, department: String) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
     }
+
     toString() {
         return `
-        id:${this._id},
+        id:${this.id},
         firstName: ${this.firstName},
         lastName: ${this.lastName},
-        email:${this.email}`;
+        email:${this.department}`;
     }
 }
