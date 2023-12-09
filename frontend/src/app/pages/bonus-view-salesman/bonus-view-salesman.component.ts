6e1c1b6266ed5f117e431694feb02fe32681f1d7
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-09 18:53:57 +0100

    adding page

---
+++ frontend/src/app/pages/bonus-view-salesman/bonus-view-salesman.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonus-view-salesman',
+  templateUrl: './bonus-view-salesman.component.html',
+  styleUrls: ['./bonus-view-salesman.component.css']
+})
+export class BonusViewSalesmanComponent {
+
+}
