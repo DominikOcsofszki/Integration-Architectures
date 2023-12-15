d0fcb2bc6ebc46cc36dae452218a33c76ffbe814
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-15 10:23:31 +0100

    test working for stubed employee api

--- backend/test/mock/test-hrm.spec.js
+++ backend/test/mock/test-hrm.spec.js
@@ -9,7 +9,8 @@ var expect = chai.expect;
 
 // const hrm = require('../test_enterprise-apps/OrangeHRMTesterWithAxios.js');
 
-const base = 'http://localhost:1337';
+// const base = 'http://localhost:1337';
+const base = 'http://localhost:8080';
 
 const responseObject = {
     statusCode: 200,
@@ -19,6 +20,16 @@ const responseObject = {
 };
 
 const responseBody = {
+    status: 'success',
+    data:[
+        {"firstName":"John","middleName":"","lastName":"Doe","code":"91338","employeeId":"85","fullName":"John Doe","status":null,"dob":null,"driversLicenseNumber":"","licenseExpiryDate":null,"maritalStatus":null,"gender":null,"otherId":"","nationality":null,"unit":"Sales","jobTitle":null,"supervisor":null},
+        {"firstName":"Paul","middleName":"","lastName":"Kaye","code":"90732","employeeId":"31","fullName":"Paul Kaye","status":null,"dob":null,"driversLicenseNumber":"","licenseExpiryDate":null,"maritalStatus":null,"gender":null,"otherId":"","nationality":null,"unit":"Sales","jobTitle":"Senior Salesman","supervisor":null},
+        {"firstName":"Mary-Ann","middleName":"","lastName":"Sallinger","code":"90124","employeeId":"9","fullName":"Mary-Ann Sallinger","status":null,"dob":null,"driversLicenseNumber":"","licenseExpiryDate":null,"maritalStatus":null,"gender":null,"otherId":"","nationality":null,"unit":"Sales","jobTitle":"Senior Salesman","supervisor":[{"name":"Michael Moore","id":" 7"}]},
+        {"firstName":"John","middleName":"Steven","lastName":"Smith","code":"90123","employeeId":"2","fullName":"John Steven Smith","status":null,"dob":"1982-11-15","driversLicenseNumber":"","licenseExpiryDate":null,"maritalStatus":null,"gender":null,"otherId":"","nationality":null,"unit":"Sales","jobTitle":"Senior Salesman","supervisor":[{"name":"Michael Moore","id":" 7"}]}
+        ,{"firstName":"Toni","middleName":"","lastName":"Tomato","code":"91337","employeeId":"84","fullName":"Toni Tomato","status":null,"dob":null,"driversLicenseNumber":"","licenseExpiryDate":null,"maritalStatus":null,"gender":null,"otherId":"","nationality":null,"unit":"Sales","jobTitle":null,"supervisor":null}
+    ],"rels":[]};
+
+const responseBody2 = {
     status: 'success',
     data: [
         {
@@ -53,13 +64,15 @@ describe('when stubbed', () => {
     afterEach(() => {
         request.get.restore();
     });
+    
 
-    describe('GET /api/v1/movies', () => {
-        it('should return all movies', (done) => {
+    //"https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/api/v1/employee/search?unit=2"
+    describe('GET /api/v1/employee', () => {
+        it('should return all employee', (done) => {
 
             this.get.yields(null, responseObject, JSON.stringify(responseBody));
 
-            request.get(`${base}/api/v1/movies`, (err, res, body) => {
+            request.get(`${base}/api/v1/employee`, (err, res, body) => {
                 // there should be a 200 status code
                 res.statusCode.should.eql(200);
                 // the response should be JSON
@@ -71,16 +84,38 @@ describe('when stubbed', () => {
                 body.status.should.eql('success');
                 // the JSON response body should have a
                 // key-value pair of {"data": [3 movie objects]}
-                body.data.length.should.eql(3);
-                // the first object in the data array should
-                // have the right keys
-                body.data[0].should.include.keys(
-                    'id', 'name', 'genre', 'rating', 'explicit'
-                );
+                body.data.length.should.eql(5);
+                
                 // the first object should have the right value for name
-                body.data[0].name.should.eql('The Land Before Time');
+                body.data[0].firstName.should.eql('John');
                 done();
             });
         });
     });
+
+// describe('GET /api/v1/employee', () => {
+//     it('should have all keys', (done) => {
+
+//         this.get.yields(null, responseObject, JSON.stringify(responseBody));
+
+//         request.get(`${base}/api/v1/employee`, (err, res, body) => {
+//             body = JSON.parse(body);
+//             body.data[0].should.contain('firstName')
+//             console.log(body.data[0].firstName)
+//             done();
+//         });
+//     });
+// });
+
+// the first object in the data array should
+                // have the right keys
+                // body.data[0].should.include.keys(
+                //     'code' ,'dob' ,'driversLicenseNumber' ,'employeeId' ,'firstName' ,'fullName' ,'gender' ,'jobTitle' ,'lastName' ,'licenseExpiryDate' ,'maritalStatus' ,'middleName' ,'nationality' ,'otherId' ,'status' ,'supervisor' ,'unit' ,'id'
+                // );
+                // [... body.data].to.have.all.keys(
+                //     'code' ,'dob' ,'driversLicenseNumber' ,'employeeId' ,
+                //     'firstName' ,'fullName' ,'gender' ,'jobTitle' ,'lastName' ,
+                //     'licenseExpiryDate' ,'maritalStatus' ,'middleName' ,'nationality' ,
+                //     'otherId' ,'status' ,'supervisor' ,'unit' ,'id'
+                // );
 });
