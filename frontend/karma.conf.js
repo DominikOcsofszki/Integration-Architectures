32257a9ef954c53b61d4d031049a6601e418a185
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-09 20:58:50 +0100

    adding remap for treesitter

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -3,16 +3,16 @@
 
 module.exports = function (config) {
     config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        basePath: "",
+        frameworks: ["jasmine", "@angular-devkit/build-angular"],
         plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-firefox-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage'),
-            require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma')
+            require("karma-jasmine"),
+            require("karma-chrome-launcher"),
+            require("karma-firefox-launcher"),
+            require("karma-jasmine-html-reporter"),
+            require("karma-coverage"),
+            require("karma-coverage-istanbul-reporter"),
+            require("@angular-devkit/build-angular/plugins/karma"),
         ],
         client: {
             jasmine: {
@@ -21,36 +21,40 @@ module.exports = function (config) {
                 // for example, you can disable the random execution with `random: false`
                 // or set a specific seed with `seed: 4321`
             },
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
+            clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
         jasmineHtmlReporter: {
-            suppressAll: true // removes the duplicated traces
+            suppressAll: true, // removes the duplicated traces
         },
         preprocessors: {
-            'src/**/!(*spec|*mock).ts': ['coverage']
+            "src/**/!(*spec|*mock).ts": ["coverage"],
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/app'),
-            subdir: '.',
+            dir: require("path").join(__dirname, "./coverage/app"),
+            subdir: ".",
             reporters: [
-                {type: 'html'},
-                {type: 'text-summary'},
-                {type: 'cobertura', subdir: '.', file: 'cobertura-coverage.xml'}
-            ]
+                { type: "html" },
+                { type: "text-summary" },
+                {
+                    type: "cobertura",
+                    subdir: ".",
+                    file: "cobertura-coverage.xml",
+                },
+            ],
         },
-        reporters: ['progress', 'kjhtml', 'coverage'],
+        reporters: ["progress", "kjhtml", "coverage"],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'Firefox'],
+        browsers: ["Chrome", "Firefox"],
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox']
-            }
+                base: "ChromeHeadless",
+                flags: ["--no-sandbox"],
+            },
         },
         singleRun: false,
-        restartOnFileChange: true
+        restartOnFileChange: true,
     });
 };
--- frontend/src/app/pages/bonus-view-salesman/bonus-view-salesman.component.ts
+++ frontend/src/app/pages/bonus-view-salesman/bonus-view-salesman.component.ts
@@ -4,55 +4,55 @@ import html2canvas from 'html2canvas';
 import { Salesman } from 'src/app/models/Salesman';
 import { BonusComputationSheet, BonusComputationSheetSchema, BonusComputationSheetModel } from 'src/app/models/BonusComputationSheet';
 @Component({
-  selector: 'app-bonus-view-salesman',
-  templateUrl: './bonus-view-salesman.component.html',
-  styleUrls: ['./bonus-view-salesman.component.css']
+    selector: 'app-bonus-view-salesman',
+    templateUrl: './bonus-view-salesman.component.html',
+    styleUrls: ['./bonus-view-salesman.component.css']
 })
 export class BonusViewSalesmanComponent {
-  title = "HR"
-  salesman: Salesman = new Salesman(1, 'John', 'Doe', 'Sales'); //ToDo delete after DB is ready
-  OneBonusComputationSheet: any;
+    title = "HR"
+    salesman: Salesman = new Salesman(1, 'John', 'Doe', 'Sales'); //ToDo delete after DB is ready
+    OneBonusComputationSheet: any;
 
-  ngOnInit(): void {
-    this.OneBonusComputationSheet = new BonusComputationSheetModel(this.salesman[0],
-    );
-    // this.fetchBonusSheetSalesman(); //ToDo add again after DB is ready
-  }
-  // constructor(private bonusService: BonusService) { }
+    ngOnInit(): void {
+        this.OneBonusComputationSheet = new BonusComputationSheetModel(this.salesman[0],
+        );
+        // this.fetchBonusSheetSalesman(); //ToDo add again after DB is ready
+    }
+    // constructor(private bonusService: BonusService) { }
 
-  // // Waiting for entries in DB!!!
-  // fetchBonusSheetSalesman(): void {
-  //   this.bonusService.getPendingSalesman().subscribe((response): void => {
-  //     if (response.status === 200) {
-  //       this.OneBonusComputationSheet = response.body;
-  //     }
-  //   });
-  // }
-
-
+    // // Waiting for entries in DB!!!
+    // fetchBonusSheetSalesman(): void {
+    //   this.bonusService.getPendingSalesman().subscribe((response): void => {
+    //     if (response.status === 200) {
+    //       this.OneBonusComputationSheet = response.body;
+    //     }
+    //   });
+    // }
 
 
 
-  generatePdf(data, id: number) {
-    html2canvas(data, { allowTaint: true }).then(canvas => {
-      let HTML_Width = canvas.width;
-      let HTML_Height = canvas.height;
-      let top_left_margin = 15;
-      let PDF_Width = HTML_Width + (top_left_margin * 2);
-      let PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-      let canvas_image_width = HTML_Width;
-      let canvas_image_height = HTML_Height;
-      let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-      canvas.getContext('2d');
-      let imgData = canvas.toDataURL("image/jpeg", 1.0);
-      let pdf = new jsPDFAll.jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-      pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
-      for (let i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage([PDF_Width, PDF_Height], 'p');
-        pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
-      }
-      const namePDF = "BonusRecords_" + id + ".pdf";
-      pdf.save(namePDF);
-    });
-  }
+
+
+    generatePdf(data, id: number) {
+        html2canvas(data, { allowTaint: true }).then(canvas => {
+            let HTML_Width = canvas.width;
+            let HTML_Height = canvas.height;
+            let top_left_margin = 15;
+            let PDF_Width = HTML_Width + (top_left_margin * 2);
+            let PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+            let canvas_image_width = HTML_Width;
+            let canvas_image_height = HTML_Height;
+            let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+            canvas.getContext('2d');
+            let imgData = canvas.toDataURL("image/jpeg", 1.0);
+            let pdf = new jsPDFAll.jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (let i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage([PDF_Width, PDF_Height], 'p');
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+            }
+            const namePDF = "BonusRecords_" + id + ".pdf";
+            pdf.save(namePDF);
+        });
+    }
 }
