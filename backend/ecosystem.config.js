aab632d89672715538e5323ac183414a70d71362
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2024-01-10 18:36:38 +0100

    changed pipeline

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
     apps: [
         {
             name: 'app',
-            script: 'src/app.js',
+            script: 'dist/app.js',
             env: {
                 NODE_ENV: 'development'
             },
