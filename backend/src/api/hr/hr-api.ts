12b7b83456747a3c620895571710c114ae63b978
mseipe2s <tr7q9Vcj9PDrw>
2024-01-10 16:23:36 +0100

    changed hr api

--- backend/src/api/hr/hr-api.ts
+++ backend/src/api/hr/hr-api.ts
@@ -1,5 +1,6 @@
 import { BonusComputationSheetModel } from "../../model/BonusComputationSheet";
 import { Request, Response } from "express";
+import { Salesman, SalesmanModel } from "../../model/Salesman";
 
 export async function readSheetStatus(req: Request, res: Response) {
     await BonusComputationSheetModel.find({})
@@ -17,6 +18,14 @@ export async function readSheetStatus(req: Request, res: Response) {
 
 export async function readPendingSheets(req: Request, res: Response) {
     await BonusComputationSheetModel.find({ status: "pending-hr" })
+        .then((value) => {
+            res.status(200).send(value);
+        })
+        .catch((reason) => res.status(400).send(reason));
+}
+
+export async function readNotPendingSheets(req: Request, res: Response) {
+    await BonusComputationSheetModel.find({ $or: [{ status: "incomplete" }, { status: "pending-ceo" }, { status: "pending-salesman" }, { status: "finished" }] })
         .then((value) => {
             res.status(200).send(value);
         })
@@ -79,4 +88,13 @@ export async function getAllSheets(req: Request, res: Response) {
             res.status(200).send(value);
         })
         .catch((reason) => res.status(400).send(reason));
+}
+
+export async function readPendingValues(req: Request, res: Response) {
+    const pendingSheets = await BonusComputationSheetModel.find({ status: "pending-hr" }).exec();
+    const salesmanList: { [id: number]: Salesman } = {};
+    for (let sheet of pendingSheets) {
+        salesmanList[sheet.salesmanId] = await SalesmanModel.find({ id: sheet.salesmanId }).exec() as unknown as Salesman;
+    }
+    const outputList: { salesmanId: number, firstname: string, lastname: string, year: number, status: string, bonus: number }[] = [];
 }
