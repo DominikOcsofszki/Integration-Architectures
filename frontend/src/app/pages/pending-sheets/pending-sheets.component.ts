517cfdd3fcf20557dffac069ebf5d1025d2e6d07
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-15 16:32:10 +0100

    adding frontend sheets

--- frontend/src/app/pages/pending-sheets/pending-sheets.component.ts
+++ frontend/src/app/pages/pending-sheets/pending-sheets.component.ts
@@ -11,26 +11,16 @@ import { PendingSalesmanService } from '../../services/pending-saleman.service';
     styleUrls: ['./pending-sheets.component.css'],
 })
 export class PendingSheetsComponent implements OnInit {
-    displayedColumns = ['Salesman', 'Firstname', 'Lastname', 'year' , 'bonus', 'status'];
+    displayedColumns = ['salesman', 'firstname', 'lastname', 'year' , 'bonus', 'status'];
 
     pendingSalesman: Salesman[] = [];
-    // pendingSalesman: Salesman[] = [new Salesman(1, 'John', 'Doe', 'Sales')];
+    restSalesmen: Salesman[] = [];
 
     constructor(private pendingSalesmanService: PendingSalesmanService) {}
 
     ngOnInit(): void {
-        // this.pendingSalesman = [
-        //     new Salesman(1, 'John', 'Doe', 'Sales'),
-        //     new Salesman(2, 'Jane', 'Doe', 'Sales'),
-        //     new Salesman(3, 'John', 'Smith', 'Sales'),
-        //     new Salesman(4, 'Jane', 'Smith', 'Sales'),
-        //     new Salesman(5, 'John', 'Doe', 'Sales'),
-        //     new Salesman(6, 'Jane', 'Doe', 'Sales'),
-        //     new Salesman(7, 'John', 'Smith', 'Sales'),
-        //     new Salesman(8, 'Jane', 'Smith', 'Sales'),
-        // ];
-        //
         this.fetchPendingSalesman(); // ToDo add again after DB is ready
+        this.fetchRestPendingSalesman(); // ToDo add again after DB is ready
     }
     // Waiting for entries in DB!!!
     fetchPendingSalesman(): void {
@@ -42,4 +32,14 @@ export class PendingSheetsComponent implements OnInit {
                 }
             });
     }
+        fetchRestPendingSalesman(): void {
+        this.pendingSalesmanService
+            .getPendingSalesman()
+            .subscribe((response): void => {
+                if (response.status === 200) {
+                    this.pendingSalesman = response.body;
+                }
+            });
+    }
+
 }
