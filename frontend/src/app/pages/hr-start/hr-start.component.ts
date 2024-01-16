edead5db9d487f7a73035fb6a6484abe3d311a04
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-16 11:43:50 +0100

    add hr, ceo, salesman

---
+++ frontend/src/app/pages/hr-start/hr-start.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hr-start',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hr-start.component.html',
+  styleUrls: ['./hr-start.component.css']
+})
+export class HrStartComponent {
+
+}
