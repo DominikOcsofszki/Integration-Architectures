811f21fe56973c285fc5e2c5d69f0c63935abcba
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-17 17:25:11 +0100

    adding routing depending on user.role, but backend using wrong user - need fix

--- frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -20,43 +20,60 @@ import { ROUTING } from '../../app.routing';
 export class MenuBarComponent implements OnInit {
 
     user: User;
-
+    buttons : any;
     /*
     This array holds the definition of the menu's buttons.
    */
-    buttons = [
-        { title: 'HR-Sheets', routerLink: ROUTING.hr.PendingSheetsComponent},
+    buttons_hr = [
+        { title: 'HR-Sheets', routerLink: ROUTING.hr.PendingSheetsComponent },
         { title: 'HR-Sheet', routerLink: ROUTING.hr.BonusViewSalesmanComponent }, // Todo change naming
         { title: 'HR-Start', routerLink: ROUTING.hr.HrStartComponent },
     ];
 
-    /**
-     * The following parameters specify objects, which will be provided by dependency injection
-     *
-     * @param authService
-     * @param router
-     * @param userService
-     */
-    constructor(private authService: AuthService, private router: Router, private userService: UserService) { }
+    buttons_admin = [
+        { title: 'admin-Sheets', routerLink: ROUTING.hr.PendingSheetsComponent },
+        { title: 'admin-Sheet', routerLink: ROUTING.hr.BonusViewSalesmanComponent }, // Todo change naming
+        { title: 'admin-Start', routerLink: ROUTING.hr.HrStartComponent },
+    ];
+    buttons_ceo = [
+        { title: 'CEO-Sheets', routerLink: ROUTING.hr.PendingSheetsComponent },
+        { title: 'CEO-Sheet', routerLink: ROUTING.hr.BonusViewSalesmanComponent }, // Todo change naming
+        { title: 'CEO-Start', routerLink: ROUTING.hr.HrStartComponent },
+    ];
+// buttons = this.buttons_hr
+/**
+ * The following parameters specify objects, which will be provided by dependency injection
+ *
+ * @param authService
+ * @param router
+ * @param userService
+ */
+constructor(private authService: AuthService, private router: Router, private userService: UserService) { }
 
-    ngOnInit(): void {
-        this.fetchUser();
-    }
+ngOnInit(): void {
+    this.fetchUser();
+    console.log(this.user);
+    // if(this.user.role) { //TODO return correct Role and not the standart user since it is not the one we are using
 
-    /**
-     * function which handles clicking the logout button
-     */
-    handleLogout(): void {
-        this.authService.logout().subscribe();
-        void this.router.navigate(['login']); // after logout go back to the login-page
-    }
+    this.buttons = this.buttons_ceo
+// }
 
-    /**
-     * fetches information about logged-in user
-     */
-    fetchUser(): void {
-        this.userService.getOwnUser().subscribe((user): void => {
-            this.user = user;
-        });
-    }
+}
+
+/**
+ * function which handles clicking the logout button
+ */
+handleLogout(): void {
+    this.authService.logout().subscribe();
+    void this.router.navigate(['login']); // after logout go back to the login-page
+}
+
+/**
+ * fetches information about logged-in user
+ */
+fetchUser(): void {
+    this.userService.getOwnUser().subscribe((user): void => {
+        this.user = user;
+    });
+}
 }
