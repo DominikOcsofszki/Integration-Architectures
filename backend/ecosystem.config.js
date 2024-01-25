b0baef4912f06407b46d8ff064f684a6bc869ecd
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-25 22:58:51 +0100

    list

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -42,7 +42,7 @@ module.exports = {
             path: TARGET_SERVER_APP_PATH,
             'post-deploy': 'pm2 list'
             // 'post-deploy': 'cd backend && npm install --production'
-            //     + ' && pm2 startOrRestart ecosystem.config.js --env=production'
+                + ' && pm2 startOrRestart ecosystem.config.js --env=production'
             //     + ' && pm2 save && cat /home/erasmux/.pm2/dump.pm2'
             // + ' && pm2 list'
             // + ' && pm2 stop default && cat /home/erasmux/.pm2/dump.pm2'
