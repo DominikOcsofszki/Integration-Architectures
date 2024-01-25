cb34676cc42c2f33a26240a138f95ad2e76f81e8
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-01-25 18:53:00 +0100

    add update call to front

--- frontend/src/app/components/sheet/sheet.component.ts
+++ frontend/src/app/components/sheet/sheet.component.ts
@@ -4,43 +4,50 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, Input,  inject } from '@angular/core';
+import { Component, OnInit, Input, inject } from '@angular/core';
 import { BonusComputationSheet } from 'src/app/models/BonusComputationSheet';
 import { SheetServiceService } from './sheet-service.service';
 import { TableComponent } from './table/table.component';
 import { TableOrderComponent } from './table-order/table-order.component';
 import { ROUTING } from 'src/app/app.routing';
-import {Router} from "@angular/router"
+import { Router } from "@angular/router"
 import { Role } from 'src/app/models/User';
+import { UpdateSheetsService } from 'src/app/services/update-sheets.service';
 
 
 @Component({
     selector: 'app-sheet',
     standalone: true,
-    imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule,TableComponent,TableOrderComponent, ReactiveFormsModule],
+    imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, TableComponent, TableOrderComponent, ReactiveFormsModule],
     templateUrl: './sheet.component.html',
     styleUrls: ['./sheet.component.css'],
 })
 export class SheetComponent implements OnInit {
     private router = inject(Router)
+    private updateSheetsService = inject(UpdateSheetsService);
     bonusComputationSheet: BonusComputationSheet;
     @Input() id: number;
     @Input() year: number;
     @Input() roleLoggedIn: Role;
-    ableToSign:boolean;
+    ableToSign: boolean;
     constructor(private sheetServiceService: SheetServiceService) { }
 
     comment = new FormControl();
 
-    isCeo(){
+    isCeo() {
         return this.roleLoggedIn === "ceo"
+    }
+
+    // (yearOfEvaluation: number, salesmanId: number, bonusComputationSheet: BonusComputationSheet)
+    updateComments() {
+        this.updateSheetsService.updateSheetsAsCeo(this.year, this.id, this.bonusComputationSheet);
     }
 
     ngOnInit() {
         this.sheetServiceService.fetchSheetFromSalesmanIdYear(this.id, this.year).
             subscribe((res: BonusComputationSheet) => {
                 this.bonusComputationSheet = res;
-                this.ableToSign = this.bonusComputationSheet.status === "pending-"+this.roleLoggedIn;
+                this.ableToSign = this.bonusComputationSheet.status === "pending-" + this.roleLoggedIn;
                 console.log(this.ableToSign)
                 this.comment.setValue(this.bonusComputationSheet.comment);
 
@@ -48,8 +55,8 @@ export class SheetComponent implements OnInit {
     }
     signCurrentSheet() {
         this.sheetServiceService.signSheetFromSalesmanIdAndYear(this.id, this.year, this.roleLoggedIn);
-        if(this.roleLoggedIn == "hr")this.router.navigate([ROUTING.hr.PendingSheetsComponent])
-        if(this.roleLoggedIn == "ceo")this.router.navigate([ROUTING.ceo.PendingSheetsComponent])
-        if(this.roleLoggedIn == "salesman")this.router.navigate([ROUTING.salesman.PendingSheetsComponent])
+        if (this.roleLoggedIn == "hr") this.router.navigate([ROUTING.hr.PendingSheetsComponent])
+        if (this.roleLoggedIn == "ceo") this.router.navigate([ROUTING.ceo.PendingSheetsComponent])
+        if (this.roleLoggedIn == "salesman") this.router.navigate([ROUTING.salesman.PendingSheetsComponent])
     }
 }
