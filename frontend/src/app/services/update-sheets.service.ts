807dca843122505617ebd5e65945757e48f3d11b
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-02-03 09:45:46 +0100

    Connect api to update bonusSheet

--- frontend/src/app/services/update-sheets.service.ts
+++ frontend/src/app/services/update-sheets.service.ts
@@ -11,31 +11,23 @@ import { Role } from '../models/User';
 export class UpdateSheetsService {
     private http = inject(HttpClient)
 
+    //TODO Marek: Api + check if boddy correct or how to put it into the post, also the link to it
 
-    //TODO: Update comment for all entries
-    //TODO Update each comment
-    //TODO update the ratings
-    //TODO something else?
-    // "/pending/sheet/comment/:salesmanId/:yearOfEvaluation"
-
-    // export type Comment = {
-    //     type: "Order" | "SocialAttribute" | "BonusComputationSheet";
-    //     _id?: String;
-    //     text: String;
-    // };
-
-    extractCommentsFromBonusComputationSheet(bonusComputationSheet: BonusComputationSheet) {
-        //TODO
-    }
-
-    updateSheetsAsCeo(yearOfEvaluation: number, salesmanId: number, bonusComputationSheet: BonusComputationSheet): Observable<HttpResponse<[]>> {
-        const comments = this.extractCommentsFromBonusComputationSheet(bonusComputationSheet);
-        //TODO
-        const body = comments //TODO to json?
+    updateSheetAsCeo(yearOfEvaluation: number, salesmanId: number, bonusComputationSheet: BonusComputationSheet): Observable<HttpResponse<[]>> {
+        const body = bonusComputationSheet //TODO to json?
         return this.http.post<[]>(
-            environment.apiEndpoint + `/api/ceo/pending/sheet/comment/${salesmanId}/${yearOfEvaluation}`,
+            environment.apiEndpoint + `/api/ceo/pending/sheet/xxx/${salesmanId}/${yearOfEvaluation}`,
             body, { observe: 'response', withCredentials: true }
         );
     }
+
+    updateSheetAsHr(yearOfEvaluation: number, salesmanId: number, bonusComputationSheet: BonusComputationSheet): Observable<HttpResponse<[]>> {
+        const body = bonusComputationSheet //TODO to json?
+        return this.http.post<[]>(
+            environment.apiEndpoint + `/api/hr/pending/sheet/xxx/${salesmanId}/${yearOfEvaluation}`,
+            body, { observe: 'response', withCredentials: true }
+        );
+    }
+
 
 }
