96753dbd197354063cc55fc3d5689da41cec30a9
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2024-02-13 15:58:58 +0100

    cleaned hr/index

--- backend/src/api/hr/index.ts
+++ backend/src/api/hr/index.ts
@@ -1,6 +1,3 @@
-/*
-    still missing: Post /setComputationDate ?
-*/
 import { Router } from "express";
 import {
     readPendingSheets,
@@ -19,18 +16,12 @@ import {
 
 const router = Router();
 
-router.post("/pending/sheet/sign/:salesmanId/:yearOfEvaluation", signSheet);
-
 router.get("/sheet/:salesmanId/:yearOfEvaluation/", getSheetByIdAndYear);
-//router.get("/sheet/:salesmanId/", getSheetsById);
-//router.get("/sheet/year/:yearOfEvaluation/", getSheetsByYear);
 router.get("/sheet", getAllSheets);
-
 router.get("/sheets/pending", readPendingValues);
 router.get("/sheets/notpending", readNotPendingValues);
 router.post("/sheets/start/:year", startBonusCalculation);
-//TODO: impl update sheet for HR
+router.post("/pending/sheet/sign/:salesmanId/:yearOfEvaluation", signSheet);
 router.post("/sheet/update", updateSheet);
-
 
 export default router;
